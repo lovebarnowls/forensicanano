360c583b8e4b01e37d63a4a849ed56f33a47c019
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-12-02 09:19:10 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-sudo useradd occamrazor
+sudo useradd pythagoras
 
-sudo mkdir -p /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/dir5
+sudo mkdir -p /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/cosinerule
 
-#MD5 hash Q1
-sudo echo 'lawofparsimony' > /home/occamrazor/Desktop/dir1/q1.txt
+#Q1 MD5hash
+sudo echo 'hypotheneuse' > /home/pythagoras/Desktop/triangle/sin/q1.txt
 
-#base64 decrypt Q2
-sudo echo 'bm92YWN1bGEgT2NjYW1p' > /home/occamrazor/Desktop/dir1/q2.txt
+#Q2 MD5hash
+sudo echo 'squared' > /home/pythagoras/Desktop/triangle/sin/cos/q2.txt
 
-#Binary Q3
-sudo echo '0111011101101000011110010110100101110011011101000110100001101001011100110110011101110101011110010111010001100001011011000110101101101001011011100110011101100001011000100110111101110101011101000110111101100011011000110110000101101101' > /home/occamrazor/Desktop/dir1/dir2/q3.txt
+#Q3 base64decode
+sudo echo 'equals' > /home/pythagoras/Desktop/triangle/sin/cos/tan/q3.txt
 
-#MD5 + base64 Q4
-sudo echo 'aG93YWJvdXRhdXRob3Jjb25hbmRveWxl' > /home/occamrazor/Desktop/dir1/dir2/q4.txt
+#MDQ4 base64decode
+sudo echo 'sumof' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/q4.txt
 
-#reverse MD5 Q5
-sudo echo '==wcllGdpxWail2cz9GcsxWYkVGdh5WatlGblVmdhhWdvlnblh2d' > /home/occamrazor/Desktop/dir1/dir2/dir3/q5.txt
+#Q5 base64encode
+sudo echo 'squareof' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/q5.txt
 
-#Morse Q6
-sudo echo '100 1111 10 0 1 1110 1 101 101 1 00 10 11 01 111 01 000 00 10 0 0 1 101 1111 000 100 11 00 1001 000 111 111 11 0111 1011 1' > /home/occamrazor/Desktop/dir1/dir2/dir3/q6.txt
+#MQ6 base64encode
+sudo echo 'othertwosides' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/q6.txt
 
-# Hidden file to decode Q7
-sudo echo 'rzxygjymjywzym' > /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/q7.txt
-sudo mv /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/q7.txt /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/.q7.txt
+#Q7 morse
+sudo echo 'onlyfortriangleswith' > /home/pythagoras/Desktop/triangle/q7.txt
 
-#cat with spaces in file name to MD5
-sudo echo '1011 111 001 11 01 100 0 00 1 1 111 1 0000 0 0 10 100' >  /home/occamrazor/Desktop/dir1/dir2/dir3/dir4/dir5/the\ end.txt
+
+#Q8 binary
+sudo echo 'rightangle' >  /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/cosinerule/q8.txt
 
 
