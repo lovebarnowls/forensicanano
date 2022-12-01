49dcf6c91a71362b5edc0d83f6f6d84118ec45eb
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-12-02 09:54:23 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -7,26 +7,30 @@ sudo mkdir -p /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/cosi
 #Q1 MD5hash
 sudo echo 'hypotheneuse' > /home/pythagoras/Desktop/triangle/sin/q1.txt
 
-#Q2 MD5hash
+#Q2 Sha256
 sudo echo 'squared' > /home/pythagoras/Desktop/triangle/sin/cos/q2.txt
 
+
 #Q3 base64decode
-sudo echo 'equals' > /home/pythagoras/Desktop/triangle/sin/cos/tan/q3.txt
+sudo echo 'ZXF1YWxz' > /home/pythagoras/Desktop/triangle/sin/cos/tan/q3.txt
+
 
 #MDQ4 base64decode
-sudo echo 'sumof' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/q4.txt
+sudo echo 'c3Vtb2Y=' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/q4.txt
 
 #Q5 base64encode
 sudo echo 'squareof' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/q5.txt
 
-#MQ6 base64encode
-sudo echo 'othertwosides' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/q6.txt
 
-#Q7 morse
-sudo echo 'onlyfortriangleswith' > /home/pythagoras/Desktop/triangle/q7.txt
+#MQ6 morse
+sudo echo '000 0 1111 1 101 0 100 000 111 11 011 1 111' > /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/q6.txt
+
+#Q7 binary
+sudo echo '0110111101101110011011000111100101100110011011110111001001110100011100100110100101100001011011100110011101101100011001010111001101110111011010010111010001101000' > /home/pythagoras/Desktop/triangle/q7.txt
 
 
-#Q8 binary
-sudo echo 'rightangle' >  /home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/cosinerule/q8.txt
+#Q8 Caesar Cipher
+sudo echo 'arightangle' | tr a-z c-za-b > 
+/home/pythagoras/Desktop/triangle/sin/cos/tan/sec/cosec/cotan/cosinerule/q8.txt
 
 
